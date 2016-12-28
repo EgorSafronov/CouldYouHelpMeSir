5414a4222d77c5c2b4d022b04ed758aa56acaf35
xRoms <romka_sm@mail.ru>
2016-12-28 06:47:36 +0300

    slsl

--- app/src/main/java/android_project/couldyouhelpmesir/SettingsActivity.java
+++ app/src/main/java/android_project/couldyouhelpmesir/SettingsActivity.java
@@ -22,8 +22,7 @@ import android.widget.ScrollView;
 import android.widget.Spinner;
 
 public class SettingsActivity extends AppCompatActivity
-        implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener,
-        Spinner.OnItemSelectedListener {
+        implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
     EditText first_name;
     EditText second_name;
     EditText email;
@@ -32,11 +31,18 @@ public class SettingsActivity extends AppCompatActivity
     Spinner spinner_city;
     ScrollView scrollView;
     SharedPreferences mSettings;
+    Integer Country = 0;
+    Integer City = 0;
+
+    public static String[] Countries = {"", "Россия", "Франция", "Испания", "Германия"};
+    public static String[][] Cities = {{}, {"Москва", "Санкт-Петербург", "Новосибирск", "Челябинск"}, {"Париж", "Лион", "Ницца", "Бордо"}, {"Мадрид", "Барселона", "Севилья", "Малага"}, {"Берлин", "Гамбург", "Мюнхен", "Дортмунд"}};
 
     public static final String DATA_FIRST_NAME = "first_name";
     public static final String DATA_SECOND_NAME = "second_name";
     public static final String DATA_EMAIL = "email";
     public static final String DATA_PHONE_NUMBER = "phone_number";
+    public static final String DATA_COUNTRY= "country";
+    public static final String DATA_CITY = "city";
 
     public android.support.v7.widget.Toolbar toolbar;
 
@@ -58,11 +64,16 @@ public class SettingsActivity extends AppCompatActivity
         phone_number = (EditText) findViewById(R.id.phone_number);
         phone_number.setText(mSettings.getString(DATA_PHONE_NUMBER, ""));
 
+        ArrayAdapter<String> adapter_coutries = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Countries);
+        adapter_coutries.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_country = (Spinner) findViewById(R.id.spinner_country);
-        spinner_country.setOnItemSelectedListener(this);
+        spinner_country.setAdapter(adapter_coutries);
+        spinner_country.setSelection(mSettings.getInt(DATA_COUNTRY, 0));
+
 
         spinner_city = (Spinner) findViewById(R.id.spinner_city);
-//        spinner_city.setOnItemClickListener(this);
+ //       spinner_city.setOnItemClickListener(this);
+        spinner_city.setVisibility(View.GONE);
 
         Button save = (Button) findViewById(R.id.save);
         save.setOnClickListener(this);
@@ -77,6 +88,31 @@ public class SettingsActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         scrollView = (ScrollView) findViewById(R.id.settings_scroll_view);
+
+
+        spinner_country.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                spinner_city.setVisibility(View.VISIBLE);
+                ArrayAdapter<String> adapter_cities = new ArrayAdapter<String>(SettingsActivity.this, android.R.layout.simple_spinner_item, Cities[position]);
+                adapter_cities.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner_city.setAdapter(adapter_cities);
+                Country = position;
+                spinner_city.setSelection(mSettings.getInt(DATA_CITY, 0));
+                spinner_city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        City = position;
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {}
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
     }
 
     @Override
@@ -97,6 +133,9 @@ public class SettingsActivity extends AppCompatActivity
 //        editor.putString("id", second_name.getText().toString());
 
         editor.putBoolean(MainActivity.FILLED_PERSONAL_INFORMATION, flag_filled);
+
+        editor.putInt(DATA_COUNTRY, Country);
+        editor.putInt(DATA_CITY, City);
 
         editor.commit();
         if (flag_filled) finish();
@@ -120,9 +159,6 @@ public class SettingsActivity extends AppCompatActivity
         } else if (id == R.id.new_request) {
             startActivity(new Intent(this, MainActivity.class));
 
-        } else if (id == R.id.users_requests) {
-            startActivity(new Intent(this, UsersRequestsActivity.class));
-
         } else if (id == R.id.request_list) {
             startActivity(new Intent(this, RequestListActivity.class));
         }
@@ -132,31 +168,4 @@ public class SettingsActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-//        String country = spinner_country.getSelectedItem().toString();
-//        Log.d("!!!",  country);
-//        spinner_city = (Spinner) findViewById(R.id.spinner_city);
-//        ArrayAdapter<CharSequence> adapter;
-//        if (country == "Германия") {
-//            adapter = ArrayAdapter.createFromResource(this, R.array.city_german, R.layout.support_simple_spinner_dropdown_item);
-//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//            spinner_city.setAdapter(adapter);
-
-//        }
-
-//        //Установка слушателя для выпадающего списка
-//        Spinner spinner = (Spinner) findViewById(R.id.spinner_city);
-//        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемента spinner
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, , android.R.layout.simple_spinner_item);
-//        // Определяем разметку для использования при выборе элемента
-//        // Применяем адаптер к элементу spinner
-//        spinner.setAdapter(adapter);
-//         
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
 }
